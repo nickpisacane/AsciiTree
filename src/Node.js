80b1c548c90cf60ba39768ad337dca992953d3f9
Nick Pisacane <pisacanen@gmail.com>
2018-02-12 19:26:59 -0800

    change the Node implementation

--- src/Node.js
+++ src/Node.js
@@ -1,82 +1,112 @@
 // @flow
 
-// TreeNode is the node-type used internally.
-export interface TreeNode {
-  parent: TreeNode | null;
-  value: string;
-  children: TreeNode[];
-  width: number;
-  left: number;
-}
-
 // NodeObj, and Node, are simplified nodes that will be accepted as inputs
 // in the primary API.
-export interface NodeObj {
+export interface SimpleNodeObj {
   value: string;
   children?: Array<Node>;
 }
 
-// @see: NodeObj
-export type Node = string | NodeObj;
+// @see: SimpleNodeObj
+export type SimpleNode = string | SimpleNodeObj;
 
-/**
- * Convert a `Node` to a `DocumentNode`.
- */
-export function nodeToTreeNode(
-  node: Node,
-  parent?: TreeNode | null = null
-): TreeNode {
-  const docNode: TreeNode = {
-    parent,
-    value: typeof node === 'string' ? node : node.value,
-    width: 0,
-    left: 0,
-    children: [],
-  };
-
-  if (typeof node !== 'string' && node.children) {
-    docNode.children = node.children.map(c => nodeToTreeNode(c, docNode));
-  }
-
-  return docNode;
+// Options type for Node
+export type NodeOptions = {
+  position?: number;
+  parent?: Node | null;
+  value?: string;
+  children?: Array<Node>;
+  width?: number;
+  left?: number;
 }
 
-/**
- * Get the levels of a given `TreeNode`.
- */
-export function getLevels(root: TreeNode): Array<Array<TreeNode>> {
-  let queue: Array<TreeNode> = [root];
-  const ret: Array<Array<TreeNode>> = [];
-  while (queue.length) {
-    ret.push(queue);
-    const temp: Array<TreeNode> = [];
-    queue.forEach(n => {
-      temp.splice(temp.length, n.children.length, ...n.children);
-    });
-    queue = temp;
-  }
-  return ret;
-}
+// Node class.
+export class Node {
+  position: number;
+  parent: Node | null;
+  value: string;
+  children: Array<Node>;
+  width: number;
+  left: number;
 
-/**
- * Iterate through all the children of a given `TreeNode`.
- */
-export function children(root: TreeNode, visitor: (node: TreeNode) => void) {
-  const queue = [...root.children];
-  while (queue.length) {
-    const node = queue.shift();
-    visitor(node);
-    queue.splice(queue.length, node.children.length, ...node.children);
-  }
-}
+  /**
+   * Create a `Node` from a `SimpleNode`. Applied recursively to children.
+   */
+  static fromSimpleNode(
+    simpleNode: SimpleNode,
+    parent?: Node | null = null,
+    position?: number = 0
+  ): Node {
+    const options: NodeOptions = {
+      position,
+      parent,
+      value: typeof simpleNode === 'string' ? simpleNode : simpleNode.value,
+    };
 
-/**
- * Determine if a given `node` is the `FirstChild` of its parent.
- */
-export function isFirstChild(node: TreeNode): boolean {
-  if (node.parent) {
-    const c = node.parent.children;
-    return c.indexOf(node) === 0;
+    const node = new Node(options);
+
+    if (typeof simpleNode !== 'string' && simpleNode.children) {
+      node.children = simpleNode.children.map((child, i) =>
+        Node.fromSimpleNode(child, node, i));
+    }
+
+    return node;
   }
-  return true;
+
+  /**
+   * Supply optional values based on the public properties in `Node`.
+   */
+  constructor(node?: NodeOptions) {
+    this.position = 0;
+    this.parent = null;
+    this.value = '';
+    this.children = [];
+    this.width = 0;
+    this.left = 0;
+
+    Object.assign(this, node);
+  }
+
+  /**
+   * Get the previous sibling of the current `Node`, if exists,
+   * otherwise `null`.
+   */
+  prevSib(): Node | null {
+    if (this.parent && this.position > 0) {
+      return this.parent.children[this.position - 1];
+    }
+    return null;
+  }
+
+  /**
+   * Get a level representation of the `Node` and its children. The levels
+   * are represented as a matrix of `Node`s with the first level containing
+   * a simple element, the root. Each subsequent level contains the children
+   * from _ALL_ of the nodes in the preceding level.
+   */
+  getLevels(): Array<Array<Node>> {
+    let queue: Array<Node> = [this];
+    const ret: Array<Array<Node>> = [];
+    while (queue.length) {
+      ret.push(queue);
+      const temp: Array<Node> = [];
+      queue.forEach(n => {
+        temp.splice(temp.length, n.children.length, ...n.children);
+      });
+      queue = temp;
+    }
+    return ret;
+  }
+
+  /**
+   * Iterate through all of the children of a given `Node`
+   */
+  children(visitor: (node: Node) => void) {
+    const queue = [...this.children];
+    while (queue.length) {
+      const node = queue.shift();
+      visitor(node);
+      queue.splice(queue.length, node.children.length, ...node.children);
+    }
+  }
 }
---
+++ src/__tests__/Node.test.js
@@ -0,0 +1,1 @@
+import {children, nodeToTreeNode, getLevels} from '../Node';
