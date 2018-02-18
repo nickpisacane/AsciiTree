f84979d93ab32b3a5eab9367dfca0b3ed5bf2d6f
Nick Pisacane <pisacanen@gmail.com>
2018-02-17 21:19:18 -0800

    lazy layout

--- src/Tree.js
+++ src/Tree.js
@@ -21,6 +21,7 @@ export type Options = {
 export default class Tree {
   _root: Node;
   _levels: Array<Array<Node>>;
+  _dirty: boolean;
 
   space: number;
   vertical: string;
@@ -37,43 +38,9 @@ export default class Tree {
 
     this._root = Node.fromSimpleNode(options.root);
     this._levels = this._root.getLevels();
-    this._calculate();
+    this._dirty = true;
   }
-
-  /**
-   * A pretty naive "box-model" - no differentiation between margins or padding,
-   * just a padding of `space` applied to the right of every node. Widths are
-   * calculated starting at the lowest level. Additionally, there is no real
-   * relative positioning, rather, positions are propagated out to children
-   * statically.
-   */
-  _calculate() {
-    for (let i = this._levels.length - 1; i >= 0; i--) {
-      const level = this._levels[i];
-
-      for (let j = 0; j < level.length; j++) {
-        const node = level[j];
-
-        const childWidth = node.children.reduce((a, c) => a + c.width, 0);
-        node.width = Math.max(node.text.width, childWidth) + this.space;
-        node.left = 0;
-
-        // If not the first child, we have to calculate an offset and apply
-        // to all of the current `node`s children.
-        const prev = node.prevSib();
-        if (prev) {
-          const offset = prev.width + prev.left;
-          node.left = offset;
-
-          // Apply offset to children
-          node.forEachChild(child => {
-            child.left += offset;
-          });
-        }
-      }
-    }
-  }
-
+  
   /**
    * Get the horizontal alignment for a given `node`, based on `alignment`.
    * @param {Node} node The node to calculate alignmemt for.
@@ -154,10 +121,89 @@ export default class Tree {
   }
 
   /**
+   * Update a tree with `Options`.
+   * @param  {Options} options Same options as in constructor.
+   */
+  update(options: Options) {
+    if (options.root) {
+      this._root = Node.fromSimpleNode(options.root);
+      this._levels = this._root.getLevels();
+      this._dirty = true;
+    }
+
+    if (options.space) {
+      this.space = options.space;
+      this._dirty = true;
+    }
+
+    if (options.vertical) {
+      this.vertical = options.vertical;
+    }
+    if (options.horizontal) {
+      this.horizontal = options.horizontal;
+    }
+    if (options.verticalHeight) {
+      this.verticalHeight = options.verticalHeight;
+    }
+    if (options.align) {
+      this.alignment = options.align;
+    }
+  }
+
+  /**
+   * Calculate layout (box-modelish). A node's "box" is defined as the entire
+   * area in which its text and children inhabbit, plus a padding of `space` to
+   * the right. Widths and offsets for nodes are calculated starting at the
+   * bottom level, moving up. The width of a given node is defined as the
+   * width of all of its _direct_ children combined, or the width of its text
+   * (whichever is larger), plus `space` padding to the right. The offset of a
+   * given node is simply where the first character of the nodes box
+   * (from the left of the entire area) will begin. The offset is absolute, not
+   * relative to a nodes parent. The offset of a node is simply the combination
+   * of the widths of all of its previous siblings. Heights are not calculated
+   * as this can be pretty easily applied during render.
+   */
+  layout(force?: boolean = false) {
+    if (!this._dirty && !force) {
+      return;
+    }
+
+    for (let i = this._levels.length - 1; i >= 0; i--) {
+      const level = this._levels[i];
+
+      for (let j = 0; j < level.length; j++) {
+        const node = level[j];
+
+        const childWidth = node.children.reduce((a, c) => a + c.width, 0);
+        node.width = Math.max(node.text.width, childWidth) + this.space;
+        node.left = 0;
+
+        // If not the first child, we have to calculate an offset and apply
+        // to all of the current `node`s children.
+        const prev = node.prevSib();
+        if (prev) {
+          const offset = prev.width + prev.left;
+          node.left = offset;
+
+          // Apply offset to children
+          node.forEachChild(child => {
+            child.left += offset;
+          });
+        }
+      }
+    }
+
+    this._dirty = false;
+  }
+
+  /**
    * Render the tree to a string.
    * @return {String}
    */
   render(): string {
+    // Layout, if neccessary.
+    this.layout();
+
     const bitmap = new Bitmap(this.width(), this.height());
     let verticalOffset = 0;
 
@@ -169,20 +215,36 @@ export default class Tree {
       level.forEach(node => {
         if (node.children.length) {
           const start = this._alignVertical(node.children[0]);
+          // draw until the last child, or `node` -- in some instances, `node`
+          // will be wider than all of its children
           const end = Math.max(
             this._alignVertical(node.children[node.children.length - 1]),
             this._alignVertical(node)
           );
 
-          this._drawHorizontal(bitmap, y + this.verticalHeight + (levelHeight - 1), start, end);
+          this._drawHorizontal(
+            bitmap,
+            y + this.verticalHeight + (levelHeight - 1),
+            start,
+            end
+          );
 
           // Draw a vertical branch above each child.
           node.children.forEach(child => {
-            this._drawVertical(bitmap, child, y + this.verticalHeight + levelHeight);
+            this._drawVertical(
+              bitmap,
+              child,
+              y + this.verticalHeight + levelHeight
+            );
           });
 
           // Draw vertical branch below node.
-          this._drawVertical(bitmap, node, y + node.text.height, levelHeight - node.text.height);
+          this._drawVertical(
+            bitmap,
+            node,
+            y + node.text.height,
+            levelHeight - node.text.height
+          );
         }
 
         // bitmap.writeString(x, y, node.value);
