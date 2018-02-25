b68b94b54eede52e4cff8184705ad215ca128fb2
Nick Pisacane <pisacanen@gmail.com>
2018-02-24 19:27:54 -0800

    integrate header and redux

--- demo/src/index.js
+++ demo/src/index.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Provider} from 'react-redux';
 import './index.css';
 import registerServiceWorker from './registerServiceWorker';
-
+import createStore from './createStore';
 import Header from './components/Header';
 
+const store = createStore();
+
 const App = () => (
-  <div>
+  <Provider store={store}>
     <Header />
-  </div>
-)
+  </Provider>
+);
 
 ReactDOM.render(<App/>, document.getElementById('root'));
 registerServiceWorker();
