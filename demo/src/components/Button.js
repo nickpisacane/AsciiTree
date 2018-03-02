355f6d4929603caed9df8d7f1221690f98006fa9
Nick Pisacane <pisacanen@gmail.com>
2018-03-01 22:09:29 -0800

    add button component

---
+++ demo/src/components/Button.js
@@ -0,0 +1,53 @@
+// @flow
+
+import * as React from 'react';
+
+import './Button.css';
+
+type Props = {
+  label?: string;
+  icon?: string;
+  primary?: boolean;
+  secondary?: boolean;
+  round?: boolean;
+};
+
+const getClassName = (props: Props): string => {
+  const classNames = ['button'];
+  if (props.primary) {
+    classNames.push('button--primary');
+  } else if (props.secondary) {
+    classNames.push('button--secondary');
+  }
+  if (props.round) {
+    classNames.push('button--round');
+  }
+  return classNames.join(' ');
+};
+
+export default class IconButton extends React.Component<Props> {
+  render() {
+    const {
+      label,
+      icon,
+      primary,
+      secondary,
+      round,
+      ...rest
+    } = this.props;
+
+    return (
+      <button
+        className={getClassName(this.props)}
+        {...rest}
+      >
+        {!!label && (
+          <span className='button__label'>{label}</span>
+        )}
+        {!!icon && (
+          <span className={`button__icon fa fa-${icon}`} />
+        )}
+      </button>
+    );
+  }
+}
