95dafa5ca5ce13f52cddf5ab14d331bb4c48db6e
Nick Pisacane <pisacanen@gmail.com>
2018-03-01 22:10:14 -0800

    add controls component

---
+++ demo/src/components/Controls.js
@@ -0,0 +1,53 @@
+// @flow
+
+import * as React from 'react';
+import type {Options} from '../reducers/options';
+import Select from './Select';
+import IconButton from './IconButton';
+import './Controls.css';
+
+export type Props = {
+  options: Options;
+  format: 'xml' | 'json';
+  onFormatChange: (format: 'xml' | 'json') => void;
+  onOptionsChange: (options: Options) => void;
+  onReset: () => void;
+};
+
+const Control = ({label, children}: {label: string; children?: React.Node}) => (
+  <div className='control'>
+    <div className='control__label'>
+      {label}
+    </div>
+    <div className='control__action'>
+      {children}
+    </div>
+  </div>
+);
+
+export default class Controls extends React.Component<Props> {
+  render() {
+    const {
+      options,
+      format,
+      onFormatChange,
+      onOptionsChange,
+      ...rest
+    } = this.props;
+
+    return (
+      <div className='controls'>
+        <Control label='Format'>
+          <Select
+            selected={format}
+            onChange={option => onFormatChange((option.value: 'xml' | 'json'))}
+            options={[
+              {value: 'xml', label: 'xml'},
+              {value: 'json', label: 'json'},
+            ]}
+          />
+        </Control>
+      </div>
+    );
+  }
+}
