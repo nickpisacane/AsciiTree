1f9bfb0ae9bef39cdfc92392424dca160afda6c5
Nicholas Pisacane <pisacanen@gmail.com>
2018-03-10 13:45:20 -0800

    Add button to toggle controls

--- demo/src/components/Header.js
+++ demo/src/components/Header.js
@@ -3,10 +3,18 @@
 import React, { Component } from 'react';
 import './Header.css';
 
-export default class Header extends Component<{}> {
+type Props = {
+  onToggleControls: () => void;
+};
+
+export default class Header extends Component<Props> {
   render() {
     return (
       <header className="header">
+        <div
+          className='header__link fa fa-cog'
+          onClick={this.props.onToggleControls}
+        />
         <div className="header__title">
           ASCII Tree
         </div>
