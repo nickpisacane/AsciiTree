4656f1ef8281c2a0059ef1a8f48810199d2fda61
Nicholas Pisacane <pisacanen@gmail.com>
2018-03-11 00:19:46 -0800

    initial code for dmeo

---
+++ demo/src/defaultCode.js
@@ -0,0 +1,26 @@
+// @flow
+
+export default `<root>
+  <node value="Value Attribute"/>
+  <node>
+    Multi-line text
+    is rendered on
+    multiple lines!
+  </node>
+  <foo>
+    <bar />
+    <bang>
+      <bang-1/>
+      <bang-2/>
+      <bang-3/>
+      <bang-4/>
+    </bang>
+  </foo>
+  <you>
+    <get>
+      <the>
+        <message />
+      </the>
+    </get>
+  </you>
+</root>`
