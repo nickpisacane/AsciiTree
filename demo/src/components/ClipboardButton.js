048ce0b14c259b2043bc11f875c54a4b46c82460
Nick Pisacane <pisacanen@gmail.com>
2018-03-12 18:45:46 -0700

    add some ui feedback when a copy takes place

--- demo/src/components/ClipboardButton.js
+++ demo/src/components/ClipboardButton.js
@@ -4,13 +4,23 @@ import * as React from 'react';
 import ClipboardJS from 'clipboard';
 import Button from './Button';
 import type {Props as ButtonProps} from './Button';
+import './ClipboardButton.css';
 
 export type Props = ButtonProps & {
   text: string;
+  activeTime?: number;
+  className?: string;
 };
 
-export default class ClipboardButton extends React.Component<Props> {
+type State = {
+  active: boolean;
+};
+
+export default class ClipboardButton extends React.Component<Props, State> {
+  static defaultProps = { activeTime: 3000 };
+
   _clipboard: ClipboardJS;
+  state: State = { active: false };
 
   _buttonRef = (button: Button | null) => {
     if (!this._clipboard && button) {
@@ -23,8 +33,41 @@ export default class ClipboardButton extends React.Component<Props> {
     }
   };
 
+  _handleCopy = () => {
+    if (this.state.active) return;
+
+    this.setState({ active: true }, () => {
+      setTimeout(() => {
+        this.setState({ active: false });
+      }, this.props.activeTime);
+    });
+  };
+
+  componentDidMount() {
+    if (this._clipboard) {
+      this._clipboard.on('success', this._handleCopy);
+    }
+  }
+
+  componentWillUnmount() {
+    if (this._clipboard) {
+      this._clipboard.destroy();
+    }
+  }
+
   render() {
-    const {text, ...rest} = this.props;
-    return <Button {...rest} ref={this._buttonRef} />;
+    const {active} = this.state;
+    const {
+      text, // eslint-disable no-unused-vars
+      activeTime, // eslint-disable no-unused-vars
+      className,
+      ...rest
+    } = this.props;
+    let _className = `clipboard-button ${className || ''}`;
+    if (active) {
+      _className += ' clipboard-button--active';
+    }
+
+    return <Button className={_className} {...rest} ref={this._buttonRef} />;
   }
 }
