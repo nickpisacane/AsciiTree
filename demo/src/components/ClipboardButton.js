26058ba1534f172b8de6b430e8c28d21ea7d052e
Nick Pisacane <pisacanen@gmail.com>
2018-03-12 17:21:12 -0700

    remove console logging

--- demo/src/components/ClipboardButton.js
+++ demo/src/components/ClipboardButton.js
@@ -15,7 +15,6 @@ export default class ClipboardButton extends React.Component<Props> {
   _buttonRef = (button: Button | null) => {
     if (!this._clipboard && button) {
       const node = button.button();
-      console.log('node: ', node);
       if (node) {
         this._clipboard = new ClipboardJS(node, {
           text: () => this.props.text,
